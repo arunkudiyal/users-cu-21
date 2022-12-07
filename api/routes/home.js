276f51cdf80b19783f6c5f7bacdca9e58eb6cbb1
arunkudiyal <arunkudiyal@icloud.com>
2022-12-07 13:28:59 +0530

    Router changed

--- api/routes/home.js
+++ api/routes/home.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const router = express.Router
+const router = express.Router()
 
 router.get('/', (req, res) => {
     res.status(200).json( {message: 'Accessing Home Route'} )
